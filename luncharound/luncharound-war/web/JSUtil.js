5d6e69566f165bfddc68dedf933c149585fa3e4b
concas.davide@gmail.com <concas.davide@gmail.com@ddbce929-d9d9-9729-247e-aaa97e4b77a9>
2011-10-02 21:09:30 +0000

    -refix ricerca -refix inserimento -refix index

--- luncharound/luncharound-war/web/JSUtil.js
+++ luncharound/luncharound-war/web/JSUtil.js
@@ -1,20 +1,26 @@
-function submitForm(complete){
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function submitForm(){
     var geocoder;
     var results;
-    if(!complete){
-        var address = this.form.via.value+", "+ this.form.citta.value;
-    }else{
-        var address = this.form.indirizzo.value;
+    var address;
+    if (document.getElementById("via") == null) {
+        address = document.getElementById("ind").value;
     }
+    else{
+        address = document.getElementById("via").value+", "+ document.getElementById("citta").value;
+    } 
     geocoder = new google.maps.Geocoder();
     geocoder.geocode( {
         'address': address
     }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            this.form.latitudine.value = results[0].geometry.location.lat().valueOf();
-            this.form.longitudine.value = results[0].geometry.location.lng().valueOf();
-            this.form.indirizzo.value= results[0].formatted_address.valueOf();
-            alert(this.form.indirizzo.value);
+            document.getElementById("lat").value = results[0].geometry.location.lat().valueOf();
+            document.getElementById("lon").value = results[0].geometry.location.lng().valueOf();
+            document.getElementById("ind").value= results[0].formatted_address.valueOf();
             this.form.submit();
         } else {
             alert("Geocode was not successful for the following reason: " + status);
