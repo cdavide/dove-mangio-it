60934bf7f0be202a9c8e6165cb8eb8ebdf480250
wontoniii <wontoniii@ddbce929-d9d9-9729-247e-aaa97e4b77a9>
2011-12-04 04:08:57 +0000

    -Login Twitter completamente funzionante(TESTATE!!!) -Creati controlloreEvento e controlloreNews

---
+++ trunk/branchJSF/luncharound/luncharound-ejb/src/java/luncharoundpkg/ControlloreNewsLocal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luncharoundpkg;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Bovio Lorenzo, Bronzino Francesco, Concas Davide
+ */
+@Local
+public interface ControlloreNewsLocal {
+    
+}
